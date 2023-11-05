9012e3fd6f8343dc014d0a728b1a9b46b8ea4c23
Aminarune <mifels.fem@gmail.com>
2023-11-05 22:27:32 +0700

    test(tasks): add service test

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('TasksService', () => {
   let service: TasksService;
+  const mockTasksRepository = () => ({
+    getAllTasks: jest.fn(),
+    getTaskById: jest.fn(),
+  });
+  const mockUser = {
+    username: 'Ariel',
+    id: 'someId',
+    password: 'somePassword',
+    tasks: [],
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        {
+          provide: TasksService,
+          useValue: {},
+          useFactory: mockTasksRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
@@ -14,5 +31,32 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllTasks', () => {
+    it('should return an array of tasks', async () => {
+      service.getAllTasks = jest.fn().mockResolvedValue('someValue');
+      const result = await service.getAllTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('should return a task', async () => {
+      const mockTask = {
+        title: 'Test task',
+        description: 'Test desc',
+        id: 'someId',
+        status: 'OPEN',
+      };
+      service.getTaskById = jest.fn().mockResolvedValue(mockTask);
+      const result = await service.getTaskById(mockTask.id, mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('should throw an error if task is not found', async () => {
+      service.getTaskById = jest.fn().mockRejectedValue(new NotFoundException());
+      await expect(service.getTaskById(null, mockUser)).rejects.toThrow(NotFoundException);
+    });
   });
 });
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,7 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TaskStatus } from './task-status.enum';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksService {
